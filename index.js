8556b0866bd2c575f6644d3a2d8935a840644c87
Magdalena Perry <magperry16@gmail.com>
2022-03-22 19:55:24 -0400

    commented out index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const fileGenerate = require("./loop");
     
 const dreamTeam = [];
 
@@ -14,7 +15,7 @@ const initQ = [{
 }, {
     // type: "input",
     message: "What is the team manager's employee ID?",
-    name: "ID"
+    name: "id"
 }, {
     // type: "list",
     message: "What is the managers's email address?",
@@ -22,25 +23,24 @@ const initQ = [{
 }, {
     // type: "input",
     message: "What is the manager's office number?",
-    name: "office"
+    name: "officeNumber"
 }, {
     type: "list",
     message: "Would you like to add another member?",
     name: "add",
     choices: [{
             name: "Engineer",
-            value: "engineer"
+            // value: "engineer"
         },
         {
             name: "Intern",
-            value: "intern"
+            // value: "intern"
         },
         {
             name: "I'm finished",
-            value: "none"
+            // value: "none"
         }
     ],
-
 }]
 
 // question to add another employee
@@ -50,15 +50,15 @@ const employeeTypeQs = {
     name: "add",
     choices: [{
             name: "Engineer",
-            value: "engineer"
+            // value: "engineer"
         },
         {
             name: "Intern",
-            value: "intern"
+            // value: "intern"
         },
         {
             name: "I'm finished",
-            value: "none"
+            // value: "none"
         }
     ]
 }
@@ -67,49 +67,51 @@ const employeeTypeQs = {
 const internQs = [{
         type: "input",
         message: "What is the intern's name?",
-        name: "internName"
+        name: "name"
     },
     {
         type: "input",
         message: "What is the intern's email?",
-        name: "internEmail"
+        name: "email"
     }, {
         type: "input",
         message: "What is the intern's id?",
-        name: "internId"
+        name: "id"
     }, {
         type: "input",
         message: "What is the intern's school?",
-        name: "internOfficeNumber"
+        name: "school"
     }
 ]
+
 // questions for engineers
 const engineerQs = [{
         type: "input",
         message: "What is the engineer's name?",
-        name: "engineerName"
+        name: "name"
     },
     {
         type: "input",
         message: "What is the engineer's email?",
-        name: "engineerEmail"
+        name: "email"
     }, {
         type: "input",
         message: "What is the engineer's id?",
-        name: "engineerId"
-    }, {
-        type: "input",
-        message: "What is the engineer's office number?",
-        name: "engineerOfficeNumber"
+        name: "id"
     }, {
         type: "input",
         message: "What is the engineer's Github username?",
-        name: "engineerGithub"
+        name: "github"
     }
 ]
-// init function to start with managers and then check if finished
+
+// init function to start with manager question and then push the manager questions into dream Team empty array
 const init = function () {
     inquirer.prompt(initQ).then((answer) => {
+        const manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+        dreamTeam.push(initQ);
+
+        
         if (answer.add === "intern") {
             internQuestions();
         } else if (answer.add === "engineer") {
@@ -119,19 +121,25 @@ const init = function () {
         }
     })
 }
-
+// prompt the questions for intern and push into dream team array
 const internQuestions = function () {
     inquirer.prompt(internQs).then((answer) => {
+        const internArr = new Intern(answer.name, answer.id, answer.email, answer.school);
+        dreamTeam.push(internArr);
         employeeType();
     })
 }
 
+// prompt the questions for intern and push into dream team array
 const engineerQuestions = function () {
     inquirer.prompt(engineerQs).then((answer) => {
+        const engineerArr = new Engineer(answer.name, answer.id, answer.email, answer.github)
+        dreamTeam.push(engineerArr);
         employeeType();
     })
 }
 
+// prompt to render new team members
 const employeeType = function () {
     inquirer.prompt(employeeTypeQs).then((answer) => {
         if (answer.add === "intern") {
@@ -144,23 +152,10 @@ const employeeType = function () {
     })
 }
 
-
+// create file in dist
 const renderTeam = function () {
-
-    fs.writeFile("./dist/team-profiles", HTML, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("check the ./dist folder for your team-profile.html file")
-        }
-    })
-
-
-    console.log('Congratulations you are finished creating your team!')
-}
-
-
-
+    fs.writeFileSync("./dist/team-profiles.html", fileGenerate(dreamTeam), 'utf-8');
+}  
 
 
 init();
